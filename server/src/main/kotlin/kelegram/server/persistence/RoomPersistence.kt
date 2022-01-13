c49c23ea152f2826783566ac37e452559b112d5a
jeovazero <contato@jeova.ninja>
2022-01-13 01:28:07 -0300

    refactor(server): split the persistence

---
+++ server/src/main/kotlin/kelegram/server/persistence/RoomPersistence.kt
@@ -0,0 +1,40 @@
+package kelegram.server.persistence
+
+import kelegram.common.Room
+import kelegram.common.UserInfo
+import org.litote.kmongo.*
+import org.litote.kmongo.coroutine.aggregate
+
+val roomCol = database.getCollection<Room>()
+
+
+object RoomPersistence {
+    suspend fun add(roomData: Room) {
+        roomCol.insertOne(roomData)
+    }
+
+    suspend fun get(id: String, userId: String? = null): Room? {
+        if (userId != null) {
+            return roomCol.findOne(and(Room::id eq id, Room::ownerId eq userId))
+        }
+        return roomCol.findOne(Room::id eq id)
+    }
+
+    suspend fun addMember(roomId: String, memberId: String) {
+        roomCol.updateOne(Room::id eq roomId,
+            push(Room::membersIds, memberId)
+        )
+    }
+
+    suspend fun getMembers(roomId: String): List<UserInfo> {
+        return roomCol.aggregate<UserInfo>(
+            match(Room::id eq roomId),
+            lookup(from = "user",
+                localField = "membersIds",
+                foreignField = "id",
+                newAs = "members"),
+            unwind("members".projection),
+            replaceRoot("members".projection)
+        ).toList()
+    }
+}
---
+++ server/src/main/kotlin/kelegram/server/persistence/UserPersistence.kt
@@ -0,0 +1,45 @@
+package kelegram.server.persistence
+
+import kelegram.common.Room
+import kelegram.common.User
+import org.litote.kmongo.*
+import org.litote.kmongo.coroutine.aggregate
+
+val userCol = database.getCollection<User>()
+
+object UserPersistence {
+    suspend fun add(userData: User) {
+        userCol.insertOne(userData)
+    }
+
+    suspend fun get(id: String): User? {
+        return userCol.findOne(User::id eq id)
+    }
+
+    /* db.roomUser.aggregate([
+        { $match: { userId: "d7ee23ce-8288-4ba6-ac1d-9a3221d7b2bb" } },
+        { $lookup: { from: "room", localField: "roomId", foreignField: "id", as: "room" } },
+        { $unwind: "$room" },
+        { $replaceRoot: {newRoot: "$room"}}
+       ])
+     */
+    suspend fun getRooms(userId: String): List<Room> {
+        return userCol.aggregate<Room>(
+            match(User::id eq userId),
+            project(User::roomsIds),
+            lookup(from = "room",
+                localField = "roomsIds",
+                foreignField = "id",
+                newAs = "rooms"),
+            unwind("rooms".projection),
+            replaceRoot("rooms".projection)
+        ).toList()
+    }
+
+    suspend fun addRoom(userId: String, roomId: String) {
+        userCol.updateOne(User::id eq userId,
+            push(User::roomsIds, roomId)
+        )
+    }
+}
+
