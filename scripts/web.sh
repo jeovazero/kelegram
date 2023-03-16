f5f18465256ce3a03d1d6ffa046eb5fdaaa554de
jeogod <hi@jeova.ninja>
2023-03-16 00:45:29 -0300

    chore: update scripts

--- scripts/web.sh
+++ scripts/web.sh
@@ -1,7 +1,14 @@
 SERVER=http://localhost:8000
+COOKIE=.cookie
 basedir=$(dirname $0)
 case $1 in
   newAccount)
-      curl -X POST -H "Content-type: application/json" -d @$basedir/data/newAccount.json $SERVER/account
+    curl --cookie-jar .cookie -X POST -H "Content-type: application/json" -d @$basedir/data/newAccount.json -s $SERVER/account
+    ;;
+  me)
+    curl --cookie .cookie  -X GET -H "Content-type: application/json" -s $SERVER/me
+    ;;
+  logout)
+    curl --cookie .cookie  -X POST -H "Content-type: application/json" -sv $SERVER/logout
     ;;
 esac
